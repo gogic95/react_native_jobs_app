e20abb7baa884cbd055e29ab9aab45706071226c
Filip Gogic <filip.gogic@visit.com>
2023-04-06 12:05:55 +0200

    setup

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text style={{color:'red'}}>
+                CAO SVETEEEE 
+            </Text>
+        </View>
+    )
+}
+
+export default Home;
