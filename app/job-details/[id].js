33c87c2d179f3be923ee8ecc5ecd2f7dd39d88ec
Filip Gogic <filipgogic95@gmail.com>
2023-04-08 08:47:02 +0200

    company details page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -46,9 +46,28 @@ const JobDetails = () => {
                 refreshControl={<RefreshControl  refreshing={refreshing} onRefresh={onRefresh} />}
             >
                 {isLoading ? (
+
                     <ActivityIndicator size="large" color={COLORS.primary}/>
-                ) : error (
-                    <Text>Something went wrong.</Text> ) }                  
+                ) : error ? (
+
+                    <Text>Something went wrong.</Text> 
+
+                ) : data.length === 0 ? (
+                    <Text> No data.</Text>
+                ) : (
+                    <View style= {{padding: SIZES.medium, paddingBottom:100}}>
+                        <Company
+                            complanyLogo={data[0].employer_logo}
+                            jobTitle={data[0].job_title}
+                            companyName={data[0].employer_name}
+                            location={data[0].job_country}
+                        />
+
+                        <JobTabs
+                        
+                        />
+                    </View>
+                ) }                  
                 
             </ScrollView>
         </>
